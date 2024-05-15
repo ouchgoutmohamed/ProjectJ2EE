d15a481d77a18bb0251b47acf1d3246703cd87ad
ouchgout mohamed <ouchgoutmohamed@gmail.com>
2024-05-15 02:41:00 +0100

    updatin loginpage

--- src/main/webapp/UserController.java
+++ src/main/webapp/UserController.java
@@ -1,8 +1,6 @@
 package com.example.controller;
 
-
 import com.example.Model.*;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet("/UserController")
+@WebServlet("/UserController") // Make sure this mapping is correct
 public class UserController extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private UserDao userDao;
@@ -21,47 +19,46 @@ public class UserController extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getServletPath();
+        String action = request.getPathInfo(); // This gets the part of the URL after '/user/'
 
         try {
-            switch (action) {
-                case "/signup":
-                    registerUser(request, response);
-                    break;
-                case "/login":
-                    authenticateUser(request, response);
-                    break;
-                default:
-                    response.sendRedirect("login.jsp");
-                    break;
+            if ("/signup".equals(action)) {
+                registerUser(request, response);
+            } else if ("/login".equals(action)) {
+                authenticateUser(request, response);
+            } else {
+                response.sendRedirect("login.jsp");
             }
         } catch (SQLException ex) {
             throw new ServletException(ex);
         }
     }
 
+
     private void registerUser(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, IOException {
+        // Ensure these match the input names in your JSP form
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
         User user = new User(name, email, password);
         userDao.saveUser(user);
-        response.sendRedirect("login.jsp");
+        response.sendRedirect("login.jsp"); // Redirect after successful registration
     }
 
     private void authenticateUser(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, IOException {
+        // Ensure these match the input names in your JSP form
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
         User user = userDao.checkLogin(email, password);
         if (user != null) {
             request.getSession().setAttribute("user", user);
-            response.sendRedirect("userhome.jsp");
+            response.sendRedirect("userhome.jsp"); // Redirect to home page on successful login
         } else {
-            response.sendRedirect("login.jsp?error=true");
+            response.sendRedirect("login.jsp?error=true"); // Redirect back to login on failure
         }
     }
 }
--- src/main/webapp/signup.jsp
+++ src/main/webapp/signup.jsp
@@ -12,7 +12,7 @@
 <div class="container" id="container">
     <%-- Sign Up Form --%>
     <div class="form-container sign-up-container">
-    <form action="UserController" method="post">
+        <form action="signup" method="post">
             <h1>Create Account</h1>
             <%-- Social Container --%>
             <div class="social-container">
@@ -29,7 +29,7 @@
     </div>
 
     <div class="form-container sign-in-container">
-        <form action="UserController" method="post">
+        <form action="login" method="post">
             <h1>Sign in</h1>
             <%-- Social Container --%>
             <div class="social-container">
@@ -46,20 +46,20 @@
     </div>
 
     <%-- Overlay Container --%>
-        <div class="overlay-container">
-            <div class="overlay">
-                <div class="overlay-panel overlay-left">
-                    <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your personal info</p>
-                    <button class="ghost" id="signIn">Sign In</button>
-                </div>
-                <div class="overlay-panel overlay-right">
-                    <h1>Hello, Explorer!</h1>
-                    <p>Enter your personal details and start journey with us</p>
-                    <button class="ghost" id="signUp">Sign Up</button>
-                </div>
+    <div class="overlay-container">
+        <div class="overlay">
+            <div class="overlay-panel overlay-left">
+                <h1>Welcome Back!</h1>
+                <p>To keep connected with us please login with your personal info</p>
+                <button class="ghost" id="signIn">Sign In</button>
+            </div>
+            <div class="overlay-panel overlay-right">
+                <h1>Hello, Explorer!</h1>
+                <p>Enter your personal details and start journey with us</p>
+                <button class="ghost" id="signUp">Sign Up</button>
             </div>
         </div>
+    </div>
 </div>
 
 <script src="js/LoginJS.js"></script>
